a9a0494163513f851d879e505b12f449ece0b5d9
vatseek <vatseek13@rambler.ru>
2014-10-14 22:19:07 +0300

    add templates

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var http = require('http');
 var path = require('path');
 var config = require('config');
 var log = require('libs/log')(module);
+var engine = require('ejs-locals');
 
 var app = express();
 app.set('port', config.get('port'));
@@ -14,38 +15,27 @@ http.createServer(app).listen(app.get('port'), function(){
     //console.log('Express server listening on port ' + app.get('port'));
 });
 
-app.use(function(req, res, next) {
-    if (req.url !== '/') {
-        next();
-        return;
-    }
+app.engine('ejs', engine);
+app.set('views', path.join(__dirname, 'templates'));
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+if (app.get('env') == 'development') {
+    app.use(express.logger('dev'));
+} else {
+    app.use(express.logger('default'));
+}
 
-    console.log('1');
-    res.end('home');
+app.use(express.json());
+app.use(express.urlencoded());
+//app.use(express.session({ secret: 'your secret here' }));
+app.use(app.router);
+
+app.get('/', function(req, res, next){
+    res.render('index', {title: 'Hello worlds'});
 });
 
-app.use(function(req, res, next) {
-    if (req.url !== '/error') {
-        next();
-        return;
-    }
+app.use(express.static(path.join(__dirname, 'public')));
 
-    next(new Error('Error 2'));
-});
-
-app.use(function(req, res, next) {
-    if (req.url !== '/test') {
-        next();
-        return;
-    }
-
-    console.log('2');
-    res.end('test');
-});
-
-app.use(function(req, res) {
-    res.send(404, 'Not found');
-});
 
 app.use(function(err, req, res, next) {
     if (app.get('env') == 'development') {
@@ -55,23 +45,3 @@ app.use(function(err, req, res, next) {
         res.send('500', 'Error');
     }
 });
-
-//app.set('port', process.env.PORT || 3000);
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'ejs');
-//app.use(express.favicon());
-//app.use(express.logger('dev'));
-//app.use(express.json());
-//app.use(express.urlencoded());
-//app.use(express.methodOverride());
-//app.use(express.session({ secret: 'your secret here' }));
-//app.use(app.router);
-//app.use(express.static(path.join(__dirname, 'public')));
-//
-//// development only
-//if ('development' == app.get('env')) {
-//  app.use(express.errorHandler());
-//}
-//
-//app.get('/', routes.index);
-//app.get('/users', user.list);
